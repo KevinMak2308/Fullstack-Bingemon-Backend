e06747e19702a16492f33bdb2b5a688c2e5414e1
SePe19 <seba217i@stud.kea.dk>
2023-09-19 13:03:46 +0200

    Second commit - name added to User model

--- src/main/java/delta/fullstackbingemonbackend/model/User.java
+++ src/main/java/delta/fullstackbingemonbackend/model/User.java
@@ -2,16 +2,24 @@ package delta.fullstackbingemonbackend.model;
 
 public class User {
     private Integer id;
+    private String name;
 
-    public User(Integer id) {
+    public User(Integer id, String name) {
         this.id = id;
+        this.name = name;
     }
 
-    public int getUserId() {
+    public int getId() {
         return id;
     }
+    public String getName() {
+        return name;
+    }
 
-    public void setUserId(Integer id) {
+    public void setId(Integer id) {
         this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
     }
 }
