307fcd2c135fd3000347e2acab06f0f7d52e9fa8
KevinMak2308 <kevi4196@stud.kea.dk>
2023-09-19 12:10:30 +0200

    Initial commit

---
+++ src/main/java/delta/fullstackbingemonbackend/FullstackBingemonBackendApplication.java
@@ -0,0 +1,13 @@
+package delta.fullstackbingemonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackBingemonBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackBingemonBackendApplication.class, args);
+    }
+
+}
