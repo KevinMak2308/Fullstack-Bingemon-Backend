1106b45e856d56aeafdaf50956c599d3386233ac
KevinMak2308 <kevi4196@stud.kea.dk>
2023-12-18 18:41:31 +0100

    Pushing to Test new yml workflow 0.4

--- src/test/java/delta/fullstackbingemonbackend/FullstackBingemonBackendApplicationTests.java
+++ src/test/java/delta/fullstackbingemonbackend/FullstackBingemonBackendApplicationTests.java
@@ -1,6 +1,8 @@
 package delta.fullstackbingemonbackend;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
@@ -8,8 +10,11 @@ import org.springframework.test.context.TestPropertySource;
 @TestPropertySource(locations = "classpath:application.properties")
 class FullstackBingemonBackendApplicationTests {
 
+    private static final Logger logger = LoggerFactory.getLogger(FullstackBingemonBackendApplicationTests.class);
+
     @Test
     void contextLoads() {
+        logger.debug("TEST LOG HERE: ");
     }
 
 }
